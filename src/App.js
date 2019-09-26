594edb59ad39bdff972130a5e632d3f52504ace9
Prashant Dahiwadkar <prashant@fluentcode.io>
2019-09-26 14:43:47 +0530

    Used react hook with redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import Button from "./component/Button";
+//reducer
+function toggleSwitch(state = "false", action) {
+  switch (action.type) {
+    case "TOGGLE CHECKED":
+      return "true";
+    case "TOGGLE UNCHECKED":
+      return "false";
+    default:
+      return state;
+  }
+}
+function todo(state = 1, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+//store
+const store = createStore(
+  combineReducers({ toggleSwitch, todo }),
+  applyMiddleware(createLogger())
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Button />
+    </Provider>
   );
 }
 
---
+++ src/component/Button.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+const TOGGLE_CHECKED = "TOGGLE CHECKED";
+const TOGGLE_UNCHECKED = "TOGGLE UNCHECKED";
+const Button = () => {
+  const currentState = useSelector(state => {
+    return {
+      toggleSwitch: state.toggleSwitch,
+      todo: state.todo
+    };
+  });
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1 style={{ textDecoration: "underline", color: "green" }}>
+        Redux with hook Example
+      </h1>
+      <h3>Check Value : {currentState.toggleSwitch}</h3>
+      <button
+        onClick={() => {
+          if (currentState.toggleSwitch === "true") {
+            dispatch({ type: TOGGLE_UNCHECKED });
+          } else {
+            dispatch({ type: TOGGLE_CHECKED });
+          }
+        }}
+      >
+        Testing
+      </button>
+      <br />
+      <br />
+      <h3>Counting : {currentState.todo}</h3>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "DECREMENT" });
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Button;
